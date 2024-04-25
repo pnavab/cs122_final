d2a064a8db1670fd288f7dd4d749e99693c3bd1a
pnavab <114110926+pnavab@users.noreply.github.com>
2024-04-25 12:47:12 -0700

    change color text logic

--- pi.py
+++ pi.py
@@ -3,53 +3,68 @@ import os
 import subprocess
 from colorama import Fore, Back, Style, init
 import pi_json_helpers as helpers
+import webbrowser
 
 
 init()
 def color_text(text, color):
   return f'{color}{text}{Style.RESET_ALL}'
 
-add_color = color_text("'pi add <alias>'", Fore.CYAN)
-open_color = color_text("'pi open <alias>'", Fore.CYAN)
-rm_color = color_text("'pi rm <alias>'", Fore.CYAN)
+# add_color = color_text("'pi add <alias>'", Fore.CYAN)
+# open_color = color_text("'pi open <alias>'", Fore.CYAN)
+# rm_color = color_text("'pi rm <alias>'", Fore.CYAN)
+
+def get_cyan_text(text):
+    return color_text(text, Fore.CYAN)
 
 def main():
-  if len(sys.argv) > 1:
-    if sys.argv[1] == "open":
-      if len(sys.argv) == 3:
-        open(sys.argv[2])
-      elif len(sys.argv) == 4 and sys.argv[3] == "-r":
-        open(sys.argv[2], reload=True)
-      else:
-        print(color_text("Make sure your request is valid!", Fore.RED))
-        help()
-    elif sys.argv[1] == "add":
-      if len(sys.argv) < 4:
-        if sys.argv[2] == ".":
-          add_current_directory()
-        else:
-          add(sys.argv[2])
-      else:
-        help()
-    elif sys.argv[1] == "rm":
-      if len(sys.argv) < 4:
-        delete(sys.argv[2])
-      else:
-        help()
-    elif sys.argv[1] == "list":
-      list_all()
+    if len(sys.argv) > 1:
+        match sys.argv[1]:
+            case "-h":
+                help()
+            case "open":
+                if len(sys.argv) == 3:
+                    open(sys.argv[2])
+                elif len(sys.argv) == 2:
+                    print(color_text("Missing alias in command", Fore.RED))
+                elif len(sys.argv) == 4 and sys.argv[3] == "-r":
+                    open(sys.argv[2], reload=True)
+                else:
+                    print(color_text("Invalid command!", Fore.RED))
+                    help()
+            case "add":
+                if len(sys.argv) < 4:
+                    if sys.argv[2] == ".":
+                        add_current_directory()
+                    else:
+                        add(sys.argv[2])
+                else:
+                    help()
+            case "rm":
+                if len(sys.argv) < 4:
+                    delete(sys.argv[2])
+                else:
+                    help()
+            case "list":
+                list_all()
+            case "ask":
+                if len(sys.argv) < 3:
+                    search()
+                else:
+                    help()
+            case _:
+                print(color_text("Invalid command!", Fore.RED))
+                help()
     else:
-      print(color_text("Make sure your request is valid!", Fore.RED))
-      help()
-  else:
-    help()
+        help()
 
 def help():
   print("="*80)
   print("Usage:")
-  print(f"   Run {add_color} to add a directory with a new alias")
-  print(f"   Run {open_color} to open a previously added directory. Append '-r' to open in the same VScode window")
-  print(f"   Run {rm_color} to remove an existing alias")
+  print(f"   Run {get_cyan_text("'pi add <alias>'")} to add a directory with a new alias")
+  print(f"   Run {get_cyan_text("'pi open <alias>'")} to open a previously added directory. Append '-r' to open in the same VScode window")
+  print(f"   Run {get_cyan_text("'pi rm <alias>'")} to remove an existing alias")
+  print(f"   Run {get_cyan_text("'pi ask'")} to toggle the search mode")
   print("="*80)
 
 def list_all():
@@ -102,6 +117,11 @@ def delete(alias):
     print(color_text(f"Alias '{alias}' removed successfully", Fore.GREEN))
   else:
     print(color_text("That alias was not found", Fore.RED))
+
+def search():
+  query = input("Enter search query: ")
+  search_url = f"https://www.google.com/search?q={query}"
+  webbrowser.open_new_tab(search_url)
 
 if __name__ == "__main__":
   main()
