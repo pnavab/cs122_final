f35798f703670e375a769109e0c31b20b3d69f80
pnavab <114110926+pnavab@users.noreply.github.com>
2024-04-24 23:18:09 -0700

    add to windows path

---
+++ test.py
@@ -0,0 +1,28 @@
+import os
+import winreg
+import subprocess
+
+cwd = os.getcwd()
+
+key = winreg.OpenKeyEx(winreg.HKEY_CURRENT_USER, "Environment", 0, winreg.KEY_ALL_ACCESS)
+
+try:
+    path, _ = winreg.QueryValueEx(key, "PATH")
+    path = path.split(";")
+    
+    if cwd not in path:
+        path.append(cwd)
+        new_path = ";".join(path)
+        
+        # ipdate the PATH environment variable
+        winreg.SetValueEx(key, "PATH", 0, winreg.REG_EXPAND_SZ, new_path)
+        print("PATH environment variable updated successfully.")
+
+        # refresh the path
+        subprocess.run(f'setx PATH "{new_path}"', shell=True)
+        print("PATH environment variable updated persistently.")
+    else:
+        print("Current working directory is already in the PATH.")
+
+finally:
+    winreg.CloseKey(key)
